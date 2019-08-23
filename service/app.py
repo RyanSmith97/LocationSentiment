77a522a1209f5624cb0d709367f70c886aa621c5
RyanSmith <ryanjsmith0@gmail.com>
2019-08-23 23:59:40 +0100

    added react app with google map

--- service/app.py
+++ service/app.py
@@ -25,28 +25,33 @@ def index():
     # Authenticate
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
+
+    #get tweets
     tweetlist = []
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-    for tweet in tweepy.Cursor(api.search, since="2018-11-17", lang="en",
-                               geocode="55.8642,-4.2518,30km").items(5):
-        tweetlist.append(tweet.text)
+    for tweet in tweepy.Cursor(api.search, since="2019-8-20", lang="en",
+                               geocode="55.8642,-4.2518,30km", tweet_mode='extended').items(5):
+        tweetlist.append(tweet.full_text)
+
 
     client = language.LanguageServiceClient()
 
-    # The text to analyze
+    #analyse tweets
+    avgSentiment = 0
     for text in tweetlist:
-    # text = u'Hello, world!'
+        # text = u'Hello, world!'
         document = types.Document(
             content=text,
             type=enums.Document.Type.PLAIN_TEXT)
 
         # Detects the sentiment of the text
         sentiment = client.analyze_sentiment(document=document).document_sentiment
+        avgSentiment += sentiment.score
+        # print('Text: {}'.format(text))
+        # print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
 
-        print('Text: {}'.format(text))
-        print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
-
-    return jsonify({'tweets': tweetlist})
+    #return analysed data
+    return jsonify({'tweets': tweetlist, 'sentiment': avgSentiment / len(tweetlist)})
 
 
 if __name__ == '__main__':
---
+++ ui/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+         lat: -1.2884,
+         lng: 36.8233
+        }}
+      />
+    );
+  }
+}
+
+
+const MAPS_KEY = process.env.REACT_APP_MAPS_API_KEY
+export default GoogleApiWrapper({
+    apiKey: MAPS_KEY
+})(MapContainer);
