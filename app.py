67f77633766bee885170ba7350a36c2f3f79f88d
RyanSmith <ryanjsmith0@gmail.com>
2019-08-23 20:44:29 +0100

    stripped newline from key

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 
 #get auth keys
 keys = open("keys.txt", "r")
-consumer_key = keys.readline()
-consumer_secret = keys.readline()
-access_token = keys.readline()
-access_token_secret = keys.readline()
+consumer_key = keys.readline().rstrip('\n')
+consumer_secret = keys.readline().rstrip('\n')
+access_token = keys.readline().rstrip('\n')
+access_token_secret = keys.readline().rstrip('\n')
 keys.close()
 
 
@@ -22,7 +22,7 @@ def index():
     tweetlist = []
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
     for tweet in tweepy.Cursor(api.search, since="2018-11-17", lang="en",
-                               geocode="55.8642,-4.2518,30km").items(50):
+                               geocode="55.8642,-4.2518,30km").items(5):
         tweetlist.append(tweet.text)
 
     return jsonify({'tweets': tweetlist})
