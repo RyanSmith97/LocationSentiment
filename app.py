2edd07646cfdb8185222243dacfc48051bab153c
RyanSmith <ryanjsmith0@gmail.com>
2019-08-23 21:18:22 +0100

    performing sentiment analysis on tweets

--- app.py
+++ app.py
@@ -7,25 +7,11 @@ from google.cloud import language
 from google.cloud.language import enums, types
 
 # Instantiates a client
-client = language.LanguageServiceClient()
-
-# The text to analyze
-text = u'Hello, world!'
-document = types.Document(
-    content=text,
-    type=enums.Document.Type.PLAIN_TEXT)
-
-# Detects the sentiment of the text
-sentiment = client.analyze_sentiment(document=document).document_sentiment
-
-print('Text: {}'.format(text))
-print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
-
 
 
 app = Flask(__name__)
 
-#get auth keys
+# get auth keys
 keys = open("twitterKeys.txt", "r")
 consumer_key = keys.readline().rstrip('\n')
 consumer_secret = keys.readline().rstrip('\n')
@@ -42,8 +28,23 @@ def index():
     tweetlist = []
     api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
     for tweet in tweepy.Cursor(api.search, since="2018-11-17", lang="en",
-                               geocode="55.8642,-4.2518,30km").items(50):
+                               geocode="55.8642,-4.2518,30km").items(5):
         tweetlist.append(tweet.text)
+
+    client = language.LanguageServiceClient()
+
+    # The text to analyze
+    for text in tweetlist:
+    # text = u'Hello, world!'
+        document = types.Document(
+            content=text,
+            type=enums.Document.Type.PLAIN_TEXT)
+
+        # Detects the sentiment of the text
+        sentiment = client.analyze_sentiment(document=document).document_sentiment
+
+        print('Text: {}'.format(text))
+        print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
 
     return jsonify({'tweets': tweetlist})
 
